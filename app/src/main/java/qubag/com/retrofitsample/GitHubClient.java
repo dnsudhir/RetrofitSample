629134c45e33413a7d5dcdcaa3b2eccb582bd985
NERVENERD\NerveNerd <dnsudhir@gmail.com>
2017-12-07 08:45:19 +0530

    with synchronous call

--- app/src/main/java/qubag/com/retrofitsample/GitHubClient.java
+++ app/src/main/java/qubag/com/retrofitsample/GitHubClient.java
@@ -1,10 +1,13 @@
 package qubag.com.retrofitsample;
 
 import java.util.List;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface GitHubClient {
   @GET("/users/{user}/repos") Call<List<GitHubRepo>> reposForUser(@Path("user") String user);
+
+  @GET("/users/{user}/repos") Call<ResponseBody> repos(@Path("user") String user);
 }
--- app/src/main/java/qubag/com/retrofitsample/MainActivity.java
+++ app/src/main/java/qubag/com/retrofitsample/MainActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 import android.widget.Toast;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.List;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -19,7 +24,26 @@ public class MainActivity extends AppCompatActivity {
 
     listView = (ListView) findViewById(R.id.listView);
 
-    mGetRepos();
+    //mGetRepos();
+    mGetReposNew();
+  }
+
+  private void mGetReposNew() {
+    GitHubClient gitHubClient = ServiceGenerator.createService(GitHubClient.class);
+    Call<ResponseBody> call = gitHubClient.repos("fs-opensource");
+
+    CallApi callApi = new CallApi(this, call);
+    callApi.setProgressDialog(true);
+    callApi.setCheckExecute(callApi, new CallApi.OnCallComplete() {
+      @Override public void CallCompleted(boolean b, String result) {
+        if (b) {
+          Type typeToken = new TypeToken<Collection<GitHubRepo>>(){}.getType();
+          Gson gson = new Gson();
+          List<GitHubRepo> gitHubRepos = gson.fromJson(result, typeToken);
+          listView.setAdapter(new GitHubRepoAdapter(MainActivity.this, gitHubRepos));
+        }
+      }
+    });
   }
 
   private void mGetRepos() {
